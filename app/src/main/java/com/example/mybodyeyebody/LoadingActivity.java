20861f39c2cc3fd33a0f1cea9cfbd9b3dc51350a
dataenggu <dsolkim1030@gmail.com>
2020-07-17 22:15:35 +0900

    Create Loading Activity and set up an intent

---
+++ app/src/main/java/com/example/mybodyeyebody/LoadingActivity.java
@@ -0,0 +1,30 @@
+package com.example.mybodyeyebody;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        startLoading();
+    }
+
+    private void startLoading() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.e("log", "시작");
+                Intent mainIntent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, 3000);
+    }
+}
