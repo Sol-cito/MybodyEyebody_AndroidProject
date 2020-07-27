ed392a3366d84347397ab3514a82aac213feaa5c
dataenggu <dsolkim1030@gmail.com>
2020-07-27 22:19:16 +0900

    Add backbutton method

--- app/src/main/java/com/example/mybodyeyebody/MainActivity.java
+++ app/src/main/java/com/example/mybodyeyebody/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.mybodyeyebody;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private PhotoFragment photoFragment;
+    private long keyPressInterval = 1500;
+    private long backKeyPressedTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +26,15 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.container, photoFragment, "PhotoFragment");
         fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (System.currentTimeMillis() - backKeyPressedTime > keyPressInterval) {
+            backKeyPressedTime = System.currentTimeMillis();
+            Toast.makeText(this, "뒤로가기를 한 번 더 누르면 종료됩니다", Toast.LENGTH_SHORT).show();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
